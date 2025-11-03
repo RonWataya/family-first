f4d79313208686772d221fd6f12a37de17d93bfe
RonWataya <ronnexwataya0@gmail.com>
2025-11-03 19:24:45 +0200

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,7 @@
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
+  
   function toggleScrolled() {
     const selectBody = document.querySelector('body');
     const selectHeader = document.querySelector('#header');
